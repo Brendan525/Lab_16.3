9b73e77f1a81fc09812d09cb1100b8627ef654d6
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-10-10 11:34:26 -0400

    Added congrats message and edit task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lab163';
+  title = 'Lab 16-3';
+
+  choreList: ToDo[] =  [
+    {task: 'Brushing teeth and wash face', complete: true}, 
+    {task: 'Iron clothes', complete: false},
+    {task: 'Cook and eat breakfast', complete: true},
+    {task: 'Go to work', complete: false},
+    {task: 'Go home', complete: false},
+  ]
+
+
+
+completeTask(choreList: ToDo)  {
+  var element = document.getElementById("target");
+  element.classList.toggle("stylesList");
+  choreList.complete = true;
 }
+
+addTask() {
+  this.choreList.push({task: 'Go to sleep', complete: false})
+}
+
+removeTask = (index) =>  {
+  this.choreList.splice(index,1);  
+};
+
+doneTasks() {
+  if (this.choreList.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+// toggleHeader = () => {
+//   this.hideTitle = !this.hideTitle;
+// };
+
+// toggleStrike= () => {
+//   this.hideTitle = !this.hideTitle;
+// }
+
+
+// togglestylesList= () => {
+//    stylesList = !stylesList;
+// }
+
+// filterToDos = () => {
+//   this.filterToDos = this.filterToDos.filter(todo =>
+//     todo.name.includes(this.filterText)
+//   );
+//   };
+
+}
+
+interface ToDo  {
+  task: string;
+  complete: boolean; 
+  // hideTitle: boolean;
+}
+
+
+
